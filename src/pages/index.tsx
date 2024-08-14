b95d2bbe138f55c8cd9599bb23598fa51ada8ee7
Jhon Fredy <jfinfocesde@gmail.com>
2024-08-14 09:52:21 -0500

    2024-8-14

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
