839365c9c8ffaa9cd7dde531173f22f7877933ce
Jhon <jhon@gmail.com>
2024-09-24 15:58:42 -0500

    2024-9-24

--- src/theme/NotFound/Content/index.tsx
+++ src/theme/NotFound/Content/index.tsx
@@ -1,55 +1,31 @@
 import React from 'react';
-import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
 import type { Props } from '@theme/NotFound/Content';
-import Heading from '@theme/Heading';
-import Layout from '@theme/Layout';
 
 export default function NotFoundContent({ className }: Props): JSX.Element {
   return (
-    <main className={clsx('container margin-vert--xl', className)}>
-      {/* <div className="row">
+    <main className="container margin-vert--xl">
+      <div className="row">
         <div className="col col--6 col--offset-3">
-          <Heading as="h1" className="hero__title">
-            <Translate
-              id="theme.NotFound.title"
-              description="The title of the 404 page">
-              Page Not Found JF
-            </Translate>
-          </Heading>
-          <p>
-            <Translate
-              id="theme.NotFound.p1"
-              description="The first paragraph of the 404 page">
-              We could not find what you were looking for.
-            </Translate>
-          </p>
-          <p>
-            <Translate
-              id="theme.NotFound.p2"
-              description="The 2nd paragraph of the 404 page">
-              Please contact the owner of the site that linked you to the
-              original URL and let them know their link is broken.
-            </Translate>
-          </p>
-        </div>
-      </div> */}
-
-      <Layout title="Contenido próximamente disponible">
-        <main className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--6 col--offset-3">
-              <h1 className="hero__title">Contenido próximamente disponible</h1>
-              <p>Este material se activará en la fecha de la semana correspondiente.</p>
-              <p>
-                Mientras tanto, puedes volver a la{' '}
-                <a href="/">página de inicio</a> para explorar el contenido existente.
-              </p>
-              <p>Gracias por tu interés.</p>
-            </div>
+          <div className="text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480" className="mb-4">
+              <rect width="640" height="480" fill="#f8f8f8" />
+              <path d="M320 240c-44.183 0-80-35.817-80-80s35.817-80 80-80 80 35.817 80 80-35.817 80-80 80zm0-128c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z" fill="#ccc" />
+              <path d="M320 272c-70.692 0-128-57.308-128-128s57.308-128 128-128 128 57.308 128 128-57.308 128-128 128zm0-224c-52.935 0-96 43.065-96 96s43.065 96 96 96 96-43.065 96-96-43.065-96-96-96z" fill="#999" />
+              <path d="M320 304c-97.202 0-176-78.798-176-176S222.798 48 320 48s176 78.798 176 176-78.798 176-176 176zm0-320C178.953 0 48 130.953 48 272s130.953 272 272 272 272-130.953 272-272S461.047 0 320 0z" fill="#666" />
+            </svg>
+            <h1 className="hero__title">Contenido próximamente disponible</h1>
+            <p>Este material se activará en la semana correspondiente.</p>
+            <p>
+              Mientras tanto, puedes volver a la{' '}
+              <a href="/" className="text-blue-500 hover:text-blue-700">
+                página de inicio
+              </a>{' '}
+              para explorar el contenido existente.
+            </p>
+            <p>Gracias por tu interés.</p>
           </div>
-        </main>
-      </Layout>
+        </div>
+      </div>
     </main>
   );
 }
